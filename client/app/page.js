5673de0918aaf67580164b147d45edd8d6903007
gitstar-oc <chandankarom07@gmail.com>
2024-10-18 23:38:58 +0530

    Modified app.js file

--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,6 @@
 // Home.js
 "use client";
 import Navbar from "../components/Navbar";
-import React, { useState } from "react";
 import SearchBox from "../components/SearchBox"; // Import your custom search box component
 import MainAccordion from "@/components/Accordion";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
@@ -10,13 +9,14 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-neutral-950 text-gray-900 dark:text-gray-100">
       <Navbar />
-      <div className="flex flex-col mt-0 items-center justify-center pb-8">
-        <Alert>
-          <AlertTitle>Heads up!</AlertTitle>
+      <div className="justify-center flex">
+      <Alert className="w-96 bg-red-600">
           <AlertDescription>
-            You can add components to your app using the cli.
+            Please note that the first request may take time up to 2 minutes!! 
           </AlertDescription>
         </Alert>
+        </div>
+      <div className="flex flex-col mt-6 items-center justify-center pb-8 ">
         <SearchBox /> {/* Add the search box */}
         <MainAccordion />
       </div>
